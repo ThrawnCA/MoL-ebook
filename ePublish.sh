23eb9b6647b478803bfa82630c283886e8351de5
ThrawnCA <thrawn@xanadu>
2017-02-26 21:15:20 +1000

    update ePub script to strip unnecessary newline characters

--- ePublish.sh
+++ ePublish.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 DIR=`dirname $0`
+mkdir $DIR/OEBPS_tmp
+for file in `ls $DIR/OEBPS/*.*ml`; do
+  cat $file | tr -d '\n' > `echo $file | sed 's|/OEBPS/|/OEBPS_tmp/|'`
+done
+mv $DIR/OEBPS $DIR/OEBPS_orig
+mv $DIR/OEBPS_tmp $DIR/OEBPS
 zip -r $DIR/"Mother of Learning.epub" $DIR/META-INF $DIR/OEBPS $DIR/mimetype
+rm -r $DIR/OEBPS
+mv $DIR/OEBPS_orig $DIR/OEBPS
