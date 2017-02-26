557ffac732545061397d0c01c3b9f9030ab7e111
ThrawnCA <thrawn@xanadu>
2017-02-26 21:24:05 +1000

    clean old ePub before assembling new one

--- ePublish.sh
+++ ePublish.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 DIR=`dirname $0`
+ARTIFACT="$DIR/Mother of Learning.epub"
 mkdir $DIR/OEBPS_tmp
 for file in `ls $DIR/OEBPS/*`; do
   cat $file | tr -d '\n' > `echo $file | sed 's|/OEBPS/|/OEBPS_tmp/|'`
 done
 mv $DIR/OEBPS $DIR/OEBPS_orig
 mv $DIR/OEBPS_tmp $DIR/OEBPS
-zip -r $DIR/"Mother of Learning.epub" $DIR/META-INF $DIR/OEBPS $DIR/mimetype
+rm "$ARTIFACT"
+zip -r "$ARTIFACT" $DIR/META-INF $DIR/OEBPS $DIR/mimetype
 rm -r $DIR/OEBPS
 mv $DIR/OEBPS_orig $DIR/OEBPS
