d4b42e1ddb2ffc03711cf151b1730d079c8a53e9
ThrawnCA <thrawn@xanadu>
2017-02-27 20:33:12 +1000

    compact consecutive whitespace when packing ePub

--- ePublish.sh
+++ ePublish.sh
@@ -4,7 +4,7 @@ DIR=`dirname $0`
 ARTIFACT="$DIR/Mother of Learning.epub"
 mkdir $DIR/OEBPS_tmp
 for file in `ls $DIR/OEBPS/*`; do
-  cat $file | tr -d '\n' > `echo $file | sed 's|/OEBPS/|/OEBPS_tmp/|'`
+  sed 's|\s\s\s*| |g' $file | tr -d '\n' > `echo $file | sed 's|/OEBPS/|/OEBPS_tmp/|'`
 done
 mv $DIR/OEBPS $DIR/OEBPS_orig
 mv $DIR/OEBPS_tmp $DIR/OEBPS
